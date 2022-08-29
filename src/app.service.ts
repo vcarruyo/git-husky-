0c8d56830c8de31808411328ee48e1e9713c0a17
victoriano <vdcarruyorodriguez@teco.com.ar>
2022-08-29 16:50:49 -0300

    feat: test commit with husky

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { map } from 'rxjs';
 
 @Injectable()
 export class AppService {
+	constructor(private readonly httpService: HttpService) {}
+
 	getHello(): string {
 		return 'Hello World!';
+	}
+
+	findAllUsers(): any {
+		return this.httpService
+			.get('https://jsonplaceholder.typicode.com/users', {
+				headers: { Accept: 'application/json' },
+			})
+			.pipe(map((response: any) => response.data));
 	}
 }
